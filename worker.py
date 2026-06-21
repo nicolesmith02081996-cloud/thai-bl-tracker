4bacb2ab484380b33b6fe23bfc87a742c15fb2b7
nicolesmith02081996-cloud <nicolesmith02081996@gmail.com>
2026-06-21 18:21:21 -0400

    Add PostgreSQL support

--- worker.py
+++ worker.py
@@ -4,14 +4,28 @@ import sys
 
 scheduler = BlockingScheduler()
 
-def run_job():
+def run_tracker_job():
     print("Running tracker...")
-    subprocess.run([sys.executable, "run_tracker.py"])
+
+    try:
+        subprocess.run(
+            [sys.executable, "run_tracker.py"],
+            check=True
+
+        )
+    except Exception as e:
+        print("Error:", e)
+
+# Run immediately at startup
+run_tracker_job()
 
 # Run every 3 hours
-scheduler.add_job(run_job, "interval", hours=3)
+scheduler.add_job(
+    run_tracker_job,
+    "interval",
+    hours=3
+)
 
-# Run once at startup
-run_job()
+print("scheduler started")
 
 scheduler.start()
